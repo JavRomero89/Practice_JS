b968113ad3dddff6d61924321afbc503fd24372b
JavRomero89 <javieromero23@gmail.com>
2023-06-22 16:44:00 +0200

    advance

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+// Definimos una variable de tipo string
+let myString = "Hola mundo";
+
+// Definimos una variable de tipo number
+let myNumber = 42;
+
+// Definimos una variable de tipo boolean
+let myBoolean = true;
+
+// Definimos una variable de tipo null
+let myNull = null;
+
+// Definimos una variable de tipo undefined
+let myUndefined;
+
+// Definimos una variable de tipo object
+let myObject = { name: "John", age: 30 };
+
+
+
+// Comprobamos el tipo de datos de cada variable
+console.log(typeof myString); // Output: string
+console.log(typeof myNumber); // Output: number
+console.log(typeof myBoolean); // Output: boolean
+console.log(typeof myNull); // Output: object
+console.log(typeof myUndefined); // Output: undefined
+console.log(typeof myObject); // Output: object
---
+++ variable.js
@@ -0,0 +1,9 @@
+let variable1 = "valor1";
+let variable2 = 10;
+let variable3 = true;
+let variable4 = NaN;
+
+console.log(variable1); // Imprime "valor1" en la consola
+console.log(variable2); // Imprime 10 en la consola
+console.log(variable3); // Imprime true en la consola
+console.log(variable4); // Imprime un NaN
